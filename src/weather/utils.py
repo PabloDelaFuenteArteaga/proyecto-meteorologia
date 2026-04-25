920063a823cc10611c5807da00ffb1c225de822b
Pablo De la Fuente <pablodelafuentearte@gmail.com>
2026-04-26 00:20:42 +0200

    add puntos_latitud_longitud and crear_geodf functions

--- src/weather/utils.py
+++ src/weather/utils.py
@@ -1,3 +1,7 @@
+import geopandas as gpd
+from shapely.geometry import Point
+
+
 def celsius_to_fahrenheit(temp_c: float) -> float:
     """Convert a temperature from Celsius to Fahrenheit.
 
@@ -8,3 +12,23 @@ def celsius_to_fahrenheit(temp_c: float) -> float:
         The temperature in degrees Fahrenheit.
     """
     return (temp_c * 9 / 5) + 32
+
+def puntos_latitud_longitud(estaciones):
+
+    latitudes, longitudes = [], []
+
+    for latitud in estaciones["latitude"]:
+        latitudes.append(latitud)
+
+    for longitud in estaciones["longitude"]:
+        longitudes.append(longitud)
+
+    puntos = zip(longitudes, latitudes)
+
+    return puntos
+
+def crear_geodf(puntos):
+    return gpd.GeoDataFrame(
+        geometry=[Point(p) for p in puntos],
+        crs="EPSG:4326"
+    )
