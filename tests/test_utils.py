4505f6efcf4024164b9efb2c865c34a0e92fbecf
Pablo De la Fuente <pablodelafuentearte@gmail.com>
2026-04-28 01:41:49 +0200

    test_utils modify

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,29 +1,6 @@
-import np
-import pytest
+import numpy as np
+
 from weather.clustering import clustering_dbscan
-
-from weather.utils import celsius_to_fahrenheit
-
-
-class TestCelsiusToFahrenheit:
-    def test_freezing_point(self):
-        assert celsius_to_fahrenheit(0) == 32.0
-
-    def test_boiling_point(self):
-        assert celsius_to_fahrenheit(100) == 212.0
-
-    def test_negative_forty(self):
-        assert celsius_to_fahrenheit(-40) == -40.0
-
-    def test_decimal_value(self):
-        assert celsius_to_fahrenheit(37.5) == 99.5
-
-    def test_absolute_zero(self):
-        assert celsius_to_fahrenheit(-273.15) == pytest.approx(-459.67)
-
-    def test_invalid_type_raises_error(self):
-        with pytest.raises(TypeError):
-            celsius_to_fahrenheit("not a number")
 
 
 class test_clustering:
